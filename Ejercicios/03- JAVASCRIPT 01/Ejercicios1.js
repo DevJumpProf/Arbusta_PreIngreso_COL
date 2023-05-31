2e174adf31249f3a2d899e8c4bd594a4bd65735c
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-05-31 20:41:22 -0300

    ejercicios js

---
+++ Ejercicios/03- JAVASCRIPT 01/Ejercicios1.js
@@ -0,0 +1,122 @@
+/*  VARIABLES / TIPOS DE DATOS / OPERADORES / OBJETOS */
+
+/* Precalentamos: */
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = null;
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = null;
+
+// Crea una variable booleana:
+const nuevoBool = null;
+
+// Resuelve el siguiente problema matemático:
+const nuevaResta = 10 - null === 5;
+
+// Resuelve el siguiente problema matemático:
+const nuevaMultiplicacion = null * 4 === 40 ;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % 5 === null;
+ 
+ /* 01- Realizaste una compra y sabes el precio del producto (inventalo) y su iva (21)%. 
+Haz un script que te calcule el precio total que vas a pagar por tu compra.
+Te recuerdo que para calcular el total debes sumar al precio el resultado de multiplicar precio por el iva y dividir por 100. */
+
+/* Ayuda:
+Precio 100€
+Iva: 21%
+El total son 121 €. */
+
+// tu solucion: 
+
+
+/* 02- En una variable tienes el lado de un cuadrado, debes escribir un programa que te calcule el área y el perímetro del cuadrado.
+el area la calculas como lado multiplicado por lado. El perémetro es la suma de los cuatro lados. */
+
+/* Ayuda:
+Lado 40
+El area es 1600
+El perímetro es 169 */
+
+// tu solucion: 
+
+
+/* 03- Un poco de interactividad. En este ejercicio debes escribir un programa que lea el nombre del usuario y salude con un "Hola" seguido del nombre del usuario.
+Para leer el nombre usa un prompt con un texto indicativo como "Escribe tu nombre" */
+
+/*Ayuda:
+ Aparecerá el prompt y el usuario escribirá su nombre:
+Usuario escribe juan
+Hola Juan. */
+
+// tu solucion: 
+
+/* 04- En este ejercicio vas a pedir a una persona que teclee tres números enteros y el script mostrará como resultado el valor medio de los tres.
+Recuerda que la media de tres números se calcula sumando los tres y dividiendo entre 3. */
+
+/* Ayuda:
+Si la persona teclea:
+3
+2
+4
+La media de los números es 3. */
+
+// tu solucion: 
+
+
+/* 05-Escribe un programa que solicite a una persona ingresar el número de kilómetros recorridos por su coche, luego le pide el número de kilómetros recorridos. El script debe mostrar el consumo de combustible por kilómetro.
+Un problema matemático muy simple numero de litros dividido por número de kilómetros. */
+
+/*Ayuda:
+La persona teclea
+Num de litros: 20
+Num de kilómetros: 80
+Has consumido 4 l/km */
+
+// tu solucion: 
+
+
+/* 06- Cambio de unidades: en este ejercicio debes convertir a segundos una medida de tiempo dada en horas y minutos.
+Recuerda una hora son 6o minutos y cada minuto son 60 segundos. */
+
+/* Ayuda:
+horas : 2
+minutos 30
+Resultado 2*60*60 + 30*60 = 9000 segundos */
+
+/* tu solucion:  */
+
+/* 7-Una tortilla de papas lleva 200 gramos de papas por persona. Por cada kilo de papas se necesitan 5 huevos y 300 gramos de cebolla. Escribe un script que dado el número de comensales calcule las cantidades de ingredientes necesarias */
+
+/* Ayuda:
+.5 comensales,
+Se necesitará:
+1 kg de papas
+5 huevos
+300 gr de cebolla */
+
+/* tu solucion: */
+
+/* 8/No todo van ser números. Escribe un código que dado un número nos devuelva true si es un número par y false si es un número impar. No usar condiconales. */
+
+/* Ayuda: */
+/* numero: 50
+¿Es par? : true */
+
+// tu solución
+
+/* 8-vamos a crear un objeto sencillo que se usa para guardar información sobre las calificaciones de un alumno. El curso contiene tres materias: Ingles, programación y HTML, el objeto contendrá el nombre del alumno y la calificación en cada una de ellas. El script imprimirá el nombre y la media de sus calificaciones */
+
+/* ayuda: Por ejemplo, guardar las calificaciones de un alumno de nombre Juan, Inglés: 9, programacion: 8, HTML: 7. Sacará Nota media de Juan 8
+ */
+
+/* tu solucion:*/
+
+
+
+
+
+
+
---
+++ Ejercicios/03- JAVASCRIPT 01/Ejercicios3.js
@@ -0,0 +1,78 @@
+/* CICLOS FOR / WHILE / DO WHILE */
+ 
+/* 01-Usando un bucle for escribir un script que pida un valor entero y muestre en pantalla una lista de números desde el 0 al valor tecleado. Los números se separarán por comas.  */ 
+
+//ayuda:Si el usuario entra por teclado el número 5 el script devolverá la secuencia  0, 1, 2, 3, 4, 5  
+
+//solucion
+
+/* 02-Se trata de que dado un número entero el script muesre una cuenta atrás, es decir, una lista de numeros separados por coma desde el número tecleado hasta el 0. */
+
+/* ayuda: Si tecle o número 5 deberá mostarar 5,4,3,2,1. */
+
+//solucion
+/* 03-Este script debe escribir la tabla de multiplicar del número que la persona teclee. La tabla mostrará en diferentes líneas cada producto y el resultado en la forma a x b = ab.
+Recuerda que el salto de linea es el carácter "\n" */
+
+/* ayuda: Si el usuario teclea el 4 la tabla mostrará diez líneas en la forma
+4 x 1 = 4
+4 x 2 = 8... */
+
+//solucion
+
+
+/* 04-Este script la persona deberá teclear una letra y un número, y el programa mostrará una cadena formada por la letra repetida el número que haya tecleado. */
+
+/* ayuda:Si la persona teclea x y 5 el script mostrará xxxxx. O sea una cadena de 5 letras x */
+
+//solucion
+
+/* 05 Programa un script que pida a una persona que teclee 4 números (uno cada vez). Con esos números el programa deberá calcular la media aritmética. O sea la suma de los 4 números dividida entre 4 */
+
+/* ayuda: El progrma pide 4 veces a la persona que telcee un número y este escribe 5,9,10,12. El programa dirá "La media es 9" */
+
+//solucion:
+
+/* 05 Usando un bucle while escribir un script que pida un valor entero y cree una lista con los números desde el 0 al valor tecleado. Luego deberá sacar esa lista con un alert. Los números se separarán por comas.  */
+
+/*ayuda: Si le doy el número 5 pues deberá contar 0,1,2,3,4,5 */
+
+//solucion
+
+/* 06-Se trata de pedir a la persona que teclee un número entre 1 y 5, si escribe alguno que esté fuera de ese rango deberá volver a pedir el número */
+
+/* ayuda: Si tecleo 0 me dirá Por favor teclee un número entre 1 y 5. */
+
+// solucion
+
+/* 07 Escribir un script que pida una contraseña. Si coincide le devolverá el mensaje "Acceso concedido" y si no coincide le deolverá el mensaje "Acceso Denegado" . Solo falla tres veces se emitirá el mensaje "Alerta, intruso" */
+
+/* ayuda: La contraseña es por ejemplo pasar. Tecleo tres veces otra distinta: Acceso Intruso. */
+
+/* solucion
+
+//8 Crear un Programa que Pide una nota (número). Muestra la calificación según la nota:
+/* 
+0-3: Muy deficiente
+3-5: Insuficiente
+5-6: Suficiente
+6-7: Bien
+7-9: Notable
+9-10: Sobresaliente */
+
+//ayuda: UTILIZAR condicionales y DO - WHILE
+
+//solucion
+
+//9     Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
+//Retornar el valor final.
+
+//ayuda: Usar el bucle do ... while.
+
+
+/* 10 Realiza un script que le pida al usuario que telee la letra A (en mayúscula) y mientras no lo haga se lo vuelva a pedir. Usa un bulce do..while y la ventana alert para los mensajes */
+/* ayuda:
+El usuario teclea b, el programa vuelve a pedirme que teclee una letra
+*/
+
+//solucion
