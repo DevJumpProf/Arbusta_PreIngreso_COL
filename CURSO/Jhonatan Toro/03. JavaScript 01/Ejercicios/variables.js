a5ec335680dc79e0e59cb91d4bb17fae74d724d9
Jhonatan Toro <jtorova5@gmail.com>
2023-05-31 11:29:35 -0500

    Agregando archivo de clase - Variables en JS

---
+++ CURSO/Jhonatan Toro/03. JavaScript 01/Ejercicios/variables.js
@@ -0,0 +1,20 @@
+
+//var -- Define las variables de manera global
+
+var juegoFavorito = "Fifa"
+juegoFavorito = "League of legends"
+console.log(juegoFavorito)
+
+//let -- El scope es local, solo funciona en el bloque de código en el que se declaró
+
+let nombre = "Jhonatan"
+console.log(nombre)
+
+//const -- Es scope es local, declarar en mayúsculas es una buena práctica
+
+const EMAIL = "jtorova5@gmail.com"
+console.log(EMAIL)
+
+
+
+
