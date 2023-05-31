96bfe581c214b1e0662e8a213cc5034c0ad5d537
Jhonatan Toro <jtorova5@gmail.com>
2023-05-31 11:55:39 -0500

    Agregando archivo de clase - Tipos de datos en JS

---
+++ CURSO/Jhonatan Toro/03. JavaScript 01/Archivos de clase/tiposDeDatos.js
@@ -0,0 +1,32 @@
+
+//Numérico - Number
+
+let edad = 34; //Sin comillas, contiene enteros, decimales, reales...
+let precio = 150.65; //Como JS está escrito en inglés, se separa con punto
+
+//Cadena de caracteres - String
+
+let nombre = "Jhonatan";  //Mantener una consistencia en el uso de comillas es una buena práctica
+let apellido = 'Toro';
+
+//Lógicos o booleanos
+
+let leGustaJavascript = true;
+let leGustaFrontEnd = false;
+
+//Objeto - Object -- Se declara por medio de llaves
+
+let persona = {
+    nombre: "Jhonatan",
+    edad: 25,
+    leGustaElRock: true
+}
+
+//Array -- Se declara por medio de corchetes
+
+let instrumentos = ["Guitarra", "Piano", "Violín"]
+
+
+
+
+
