880566b5953291fb5b7b45e104e99aa284a5aa53
Jhonatan Toro <jtorova5@gmail.com>
2023-06-01 08:56:16 -0500

    Agregando archivo de clase - Condicionales (if) en JS

---
+++ CURSO/Jhonatan Toro/03. JavaScript 01/Archivos de clase/condicionalesIf-else.js
@@ -0,0 +1,35 @@
+
+//Condicional simple
+
+if (condicion) {
+    //Código a ejecutar si la condicion es verdadera
+}
+
+//Condicional con bloque else 
+
+if (condicion) {
+    //Código a ejecutar si la condicion es verdadera
+} else {
+    //Código a ejecutar si la condicion es falsa
+}
+
+//Condicional con bloque else if
+
+if (condicion) {
+    //Código a ejecutar si la condicion es verdadera
+} else if (condicion2) {
+    //Código a ejecutar si la condicion2 es verdadera
+} else {
+    //Código a ejecutar si la condicion2 es falsa
+}
+
+let clima = "Nublado"
+let dia = "Lunes"
+
+if ((clima == "Soleado") && (dia == "Domingo")) {
+    console.log("Voy a la pileta")
+} else if (clima == "Soleado" && dia == "Lunes") {
+    console.log("Bueh, voy a la pileta igual")
+} else {
+    console.log("Me quedo en casa mirando una peli")
+}
