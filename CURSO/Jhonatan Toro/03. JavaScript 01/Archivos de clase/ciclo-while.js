8f44dcc7cc0fb4a82f76400203d0e8884f56362d
Jhonatan Toro <jtorova5@gmail.com>
2023-06-01 16:26:03 -0500

    Agregando archivo de clase - Ciclos (while, do while) en JS

---
+++ CURSO/Jhonatan Toro/03. JavaScript 01/Archivos de clase/ciclo-while.js
@@ -0,0 +1,18 @@
+
+//While
+
+let contador = 0; 
+
+while (contador <= 4) {
+    console.log(`Contador: ${contador}`);
+    contador += 1;
+}
+
+//do while  --> Se ejecuta mínimo 1 vez
+
+let diaSemana = 3; 
+
+do {
+    console.log(`Día de la Semana: ${diaSemana}`);
+    diaSemana += 1;
+} while (diaSemana <=7 )
