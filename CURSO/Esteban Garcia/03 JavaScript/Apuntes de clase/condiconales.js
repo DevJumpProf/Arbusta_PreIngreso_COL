2fb2a9f6932eb10eaec14e8519c593c756f09c41
Esteban <egarcia9543@soy.sena.edu.co>
2023-06-21 22:07:12 -0500

    Ejercitacion 2

---
+++ CURSO/Esteban Garcia/03 JavaScript/Apuntes de clase/condiconales.js
@@ -0,0 +1,13 @@
+//condicionales
+
+let clima = 'lluvioso';
+let dia = 'lunes';
+//clima = 'soleado';
+
+if (clima == 'lluvioso' && dia == 'lunes') {
+    console.log('No quiero ir a trabajar');
+} else if (clima == 'soleado' && dia == 'lunes') {
+    console.log('Voy a ir a trabajar');
+} else {
+    console.log('No se que hacer');
+}
